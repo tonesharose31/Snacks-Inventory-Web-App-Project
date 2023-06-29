bd24a09b98ef63b6483673e1226cb54af3fd9f98
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-06-29 16:26:00 -0400

    user can now add item

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-document.getElementById("snackForm").addEventListener("submit", (event)=>{
+let form = document.getElementById("snacksForm")
+let ul= document.querySelector("ul")
+form.addEventListener("submit", (event)=>{
   event.preventDefault();
-  console.log(event.target.elements("snackName"));
-  console.log(event.target.elements("snackQuantity"));
-  console.log(event.target.elements("snackPrice"));
+
+let li = document.createElement("li")
+let name = form.querySelector("#snackName").value;
+let quantity = form.querySelector("#snackQuantity").value
+let price = form.querySelector("#price").value
+let image = form.querySelector("#image").value
+
+li.innerHTML =`name:${name}
+price: ${price}
+quantity: ${quantity}
+<img src= ${image} alt= "image"/>
+`
+ul.append(li)
 });
+
+
+
      
  
