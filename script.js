c789f0d52c69604584dfbca5420d58d13600336a
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-01 13:26:20 -0400

    fixed i can add to inventory again

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ let ul= document.querySelector("ul")
 form.addEventListener("submit", (event)=>{
   event.preventDefault();
 
-let li = document.createElement("li").value;
+let li = document.createElement("li")
 let name = form.querySelector("#snackName").value;
 let quantity = form.querySelector("#snackQuantity").value
 let price = form.querySelector("#price").value
 let image = form.querySelector("#image").value
-
+let fielset = form.querySelector("fieldset").value
 
 li.innerHTML = `<img src = ${image} alt="image" /> 
-<h6>Name: </h3> <p> ${name} </p>
-<h3>Price:<h3/> <p>${price}<p>
+<h6>name: </h3> <p> ${name} </p>
+<h3>price:<h3/> <p>${price}<p>
 <h3>quantity:</h3> <p>${quantity}</p>`
+{/* <h3>fielset:</h3> <p>${fielset}</p>// */}
 ul.append(li) ;
  });
 
