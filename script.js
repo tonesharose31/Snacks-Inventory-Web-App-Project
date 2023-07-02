8feb3c78769d1741f3a9a33f6dc6e6387a683de2
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-02 05:18:35 -0400

    user can now update in stock and out of stock status

--- script.js
+++ script.js
@@ -14,33 +14,26 @@ li.innerHTML = `<img src = ${image} alt="image" />
 <h3>name: </h3> <p> ${name} </p>
 <h3>price:<h3/> <p>${price}<p>
 <h3>quantity:</h3> <p>${quantity}</p>
-<button class="toggleButton">In Stock</button>;
-<button class="removeButton">Delete</button`;
+<button class ="toggleButton">In Stock</button>
+<button class="removeButton">Delete</button>`;
 
 ul.append(li);
 form.reset();
  });
 
-ul.addEventListener("click", (event) => {
-  if(event.target.classList.contains("removeButton")){
-    let li = event.target.closest("li");
-    li.remove();
-    
-  }  else if( event.target.classList.contains("toggleButton"))
-  { let toggleButton =event.target;
-    toggleStockStatus(toggleButton);
-  }
-});
-
-function toggleStockStatus(button){
-  let stockStatus = button.textContent;
-  button.textContent = stockStatus === "In Stock" ? "out of Stock" :"In Stock";
-}
-
-
-
-
-
-
-
-
+ 
+ ul.addEventListener("click", (event) => {
+   if(event.target.classList.contains("removeButton")){
+     let li = event.target.closest("li");
+     li.remove();
+    }  else if( event.target.classList.contains("toggleButton")){
+     let toggleButton = event.target;
+toggleStockStatus(toggleButton);
+    }
+  });
+   
+    function toggleStockStatus(button){
+    let stockStatus = button.textContent;
+    button.textContent = stockStatus === "In Stock" ? "out of Stock" : "In Stock";
+  };
+  
