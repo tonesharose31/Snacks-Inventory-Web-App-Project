564764d513a0263f02b1c6257a2454d58298e49c
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-01 22:34:48 -0400

    working on getting my toggle to work

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ form.addEventListener("submit", (event)=>{
 
 let li = document.createElement("li")
 let name = form.querySelector("#snackName").value;
-let quantity = form.querySelector("#snackQuantity").value
-let price = form.querySelector("#price").value
-let image = form.querySelector("#image").value
+let quantity = form.querySelector("#snackQuantity").value;
+let price = form.querySelector("#price").value;
+let image = form.querySelector("#image").value;
+
 
 li.innerHTML = `<img src = ${image} alt="image" /> 
 <h3>name: </h3> <p> ${name} </p>
 <h3>price:<h3/> <p>${price}<p>
 <h3>quantity:</h3> <p>${quantity}</p>
+<button class="toggleButton"<In Stock</button>;
 <button>Delete</button`;
 
 ul.append(li);
@@ -23,9 +25,16 @@ ul.addEventListener("click", (event) => {
   if(event.target.tagName.toLowerCase()=== "button"){
     let li = event.target.closest("li");
     li.remove();
+  } else if( event.target.classList.contains("toggleButton"))
+  {let toggleButton =event.target;
+    toggleStockStatus(toggleButton);
   }
-})
+});
 
+function toggleStockStatus(button){
+  let stockStatus = button.textContent;
+  button.textContent = stockStatus === "In Stock" ? "out of Stock" :"In Stock";
+}
 
 
 
