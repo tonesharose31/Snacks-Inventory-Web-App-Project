72d3c23eb07f18440f245d7fa447dd7224887f3f
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-01 20:08:12 -0400

    applied app background and padding on form

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ let name = form.querySelector("#snackName").value;
 let quantity = form.querySelector("#snackQuantity").value
 let price = form.querySelector("#price").value
 let image = form.querySelector("#image").value
-let fielset = form.querySelector("fieldset").value
 
 li.innerHTML = `<img src = ${image} alt="image" /> 
-<h6>name: </h3> <p> ${name} </p>
+<h3>name: </h3> <p> ${name} </p>
 <h3>price:<h3/> <p>${price}<p>
 <h3>quantity:</h3> <p>${quantity}</p>`
-{/* <h3>fielset:</h3> <p>${fielset}</p>// */}
 ul.append(li) ;
  });
 
+
+ 
 // const remove = document.getElementById("b")
 // removeButton.textContent= "remove snack";
 //   li.append(remove)
